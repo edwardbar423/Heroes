745364e3a5e063a978bf5b6c4c4cd1d5e7196b89
Edward Barber <luckyshot423@gmail.com>
2016-08-09 23:56:37 -0400

    Even more updates - still need a more completed project.

--- Heroes/main.c
+++ Heroes/main.c
@@ -7,6 +7,8 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     
@@ -34,24 +36,74 @@ int main(int argc, const char * argv[]) {
         scanf("%d", &action);
     }
     
-    if(action = 1) {
+    if(action == 1) {
         printf("Eyes sore, weary. The work is great but sometimes... it would be nice if it was just finished.");
     }
-    if(action = 2) {
+    if(action == 2) {
         printf("You cough, hoarse. Not really sure what brought that on. Is there a need for concern?");
     }
-    if(action = 3) {
+    if(action == 3) {
         printf("You get up and make some coffee. 'Hey!' You tell yourself, 'it gets me through the nights.'");
     }
-    if(action = 4) {
+    if(action == 4) {
         printf("Get that urge again, nature calls");
         //Something needs to go here to signify you head to the "Bathroom - room".
     }
-    if(action = 5) {
+    if(action == 5) {
         printf("Reach up high, giving the stiff muscles a flex.");
     }
-    if(action = 6) {
+    if(action == 6) {
         printf("Atlanta may not be the biggest city, but that doesn't mean it lacks beauty.");
     }
+    
+    
+    //Sleep doesn't seem to be working. Look at for review.
+    printf("BOOM, keeeeeeerrrrrrr, RrrrreeedrrrrRRRrruUUumMMmmm.");
+    sleep(3);
+    printf("The building shakes and rocks, followed by a loud boom");
+    sleep(3);
+    printf("'What was that?' you say, shaken. Catching yourself from having to fall to the floor");
+    sleep(3);
+    printf("The lights flicker and blink, then go out. Leaving you in darkness at The Iron Yard.");
+    sleep(3);
+    printf("With grimace you turn on the flashlight function on your phone. Noticing that you no longer have signal you turn your phone to airplane mode.");
+    sleep(3);
+    printf("Waiting for a moment you get your barings");
+    sleep(3);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Some pause for dramatic effect here...
+   // int action()
+    //{
+     //   system("read -n 1 -s -p \"Press any key to continue...\"");
+      //  return 0;
+    //}
+    
+    
+    
+    //#include <stdio.h>
+    //#include <conio.h>
+    //int main() {
+    //Your Code
+    //getch();
+    //return 0;
+    //}
+    
+    // also
+    
+    //#include <stdlib.h>
+    //int main() {
+    //system("read -n 1 -s -p \"Press any key to continue...\"");
+    //return 0;
+    //}
+    
+    
     return 0;
 }
